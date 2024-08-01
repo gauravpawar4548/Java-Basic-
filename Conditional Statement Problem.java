ee0a667f5b0373e1a6776fefe5199bf78701d07a
gauravpawar 4548 <gauravpawar1470@gmail.com>
2024-08-01 07:45:35 +0530

    Use Condition

---
+++ Conditional Statement Problem.java
@@ -0,0 +1,185 @@
+package com.example.myFirstProject;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Conditional {
+    public static void main(String[] args) {
+
+        // if -else condition and relational operator
+
+//        int a =6;
+//
+//        if(a>18)
+//        {
+//            System.out.println("yes its ");
+//        }
+//        else {
+//            System.out.println("No");
+//        }
+//        int c = 15;
+//        if(c!=5)
+//        {
+//            System.out.println("y");;
+//
+//        }
+//
+//          //relational operator
+//
+//        boolean x = true;
+//        boolean d=true;
+//        // && two statement true
+//        // one condition true ||
+//        // no and yes !
+//
+//
+//
+//        if(x && d)
+//        {
+//            System.out.println("y");
+//        }
+//        else {
+//            System.out.println("N");
+//        }
+//        if(x || d)
+//        {
+//            System.out.println("Y");
+//        }
+//        else {
+//            System.out.println("N");
+//        }
+//
+//        if(x!=d)
+//        {
+//            System.out.println("Y");
+//        }
+//        else {
+//            System.out.println("N");
+//        }
+
+        // Switch case statement
+//
+//         Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter your age");
+//        int age = 56;
+//        age = sc.nextInt();
+
+       // ladder if else if else if else
+
+//        if(age>56)
+//        {
+//            System.out.println("You are exprienced");
+//
+//        }
+//        else if(age>46)
+//        {
+//            System.out.println("you are Semi exprienced");
+//        }
+//        else if(age>36)
+//        {
+//            System.out.println("you are semi-semi exprienced");
+//        }
+//        else {
+//            System.out.println("you are not ");
+//        }
+
+   
+              // Using Switch case
+//              switch (age)
+//              {
+//                  case 18:
+//                      System.out.println("you are enjoy ");
+//                      break;
+//                  case 25:
+//                      System.out.println("you are ready to job");
+//                      break;
+//                  case 59:
+//                      System.out.println("you are retired");
+//                      break;
+//                  default:
+//                      System.out.println("you are death");
+////
+//
+//              }
+//        System.out.println("thank you");
+//
+
+        
+        //avg of mark
+//        int m1,m2,m3;
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Enter your mark physics");
+//        m1 = sc.nextInt();
+//        System.out.println("Enter your mark chem");
+//        m2= sc.nextInt();
+//        System.out.println("Enter your mark bio");
+//        m3 = sc.nextInt();
+//        float avg = (m1+m2+m3)/3.0f;
+//        System.out.println(avg);
+//
+//        if(avg>=40 && m1>=33 && m2>=33 && m3>=33)
+//        {
+//            System.out.println("Congratulation ");
+//        }
+//        else {
+//            System.out.println("sorry you are fail! next try please");
+//        }
+//
+
+         // find website
+//        Scanner sc = new Scanner(System.in);
+//        String websites = sc.next();
+//        if(websites.endsWith(".org"))
+//        {
+//            System.out.println("this is organised websites");
+//        }
+//        else if(websites.endsWith(".com"))
+//        {
+//            System.out.println("commercial websites is " );
+//        }else if(websites.endsWith(".in"))
+//        {
+//            System.out.println("indian websites");
+//
+//        }
+
+         //  find the day for the no.
+//        Scanner sc = new Scanner(System.in);
+//
+//        int date = sc.nextInt();
+//
+//
+//        switch(date) {
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Thuesday");
+//                break;
+//            case 3:
+//                System.out.println("wedsday");
+//                break;
+//            case 4:
+//                System.out.println("thursday");
+//                break;
+//            case 5:
+//                System.out.println("friday");
+//                break;
+//            case 6:
+//                System.out.println("saturday");
+//                break;
+//        }
+//        System.out.println("days is the show ");
+//
+        //leap year find
+//        int year =2000;
+//        if(year%4==0)
+//        {
+//            System.out.println("it is leap year");
+//        }
+//        else {
+//            System.out.println("this is not leap year");
+//        }
+
+    }
+}
